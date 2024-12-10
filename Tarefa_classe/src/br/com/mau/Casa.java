941a2c1cdb5f254ee1cf0c52bf114a051df74839
Mauricio <mauriciosed18@gmail.com>
2024-12-10 17:58:59 -0300

    tarefa cafe da manha

---
+++ Tarefa_classe/src/br/com/mau/Casa.java
@@ -0,0 +1,14 @@
+package br.com.mau;
+
+public class Casa {
+
+	public static void main(String[] args) {
+		Familia familia = new Familia ();
+		int numeroQuarto = familia.getnunQuarto(0);
+		System.out.println("Numero do quarto: " + numeroQuarto);
+		
+		
+		
+	}
+
+}
---
+++ Tarefa_classe/src/br/com/mau/Familia.java
@@ -0,0 +1,41 @@
+package br.com.mau;
+
+public class Familia {
+
+	private String nome;
+	
+	private int idade;
+	
+	private int quarto;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getQuarto() {
+		return quarto;
+	}
+
+	public void setQuarto(int quarto) {
+		this.quarto = quarto;
+	}
+	/**
+	 * informar numero do quarto
+	 */
+	public int getnunQuarto(int count) {	
+	 return 2;
+	
+	}
+}
