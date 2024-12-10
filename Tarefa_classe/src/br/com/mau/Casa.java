ff01d98527435832f91fd7b12690aa7f0a4fa989
Mauricio <mauriciosed18@gmail.com>
2024-12-10 18:11:14 -0300

    tarefa mod7

--- Tarefa_classe/src/br/com/mau/Casa.java
+++ Tarefa_classe/src/br/com/mau/Casa.java
@@ -5,6 +5,10 @@ public class Casa {
 	public static void main(String[] args) {
 		Familia familia = new Familia ();
 		int numeroQuarto = familia.getnunQuarto(0);
+	
+		/**
+		 * Numero do quarto
+		 */
 		System.out.println("Numero do quarto: " + numeroQuarto);
 		
 		
