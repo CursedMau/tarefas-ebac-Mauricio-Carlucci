e11c260d6f8423b9889cd24251e0f7bb58da344e
Mauricio <mauriciosed18@gmail.com>
2024-12-17 11:58:15 -0300

    Tarefa Wrapper

---
+++ Tarefa_wrapper/src/br/com/mau/Wrapper.java
@@ -0,0 +1,20 @@
+package br.com.mau;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		System.out.println(num1);
+		
+		int num2 = Integer.valueOf(num1);
+		System.out.println(num2);
+		
+				
+		
+		
+		
+
+	}
+
+}
